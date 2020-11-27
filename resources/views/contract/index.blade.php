304613184d926b4e6a3ebd55a4a9226e04b2b10a
AnamariaBianca <73616652+AnamariaBianca@users.noreply.github.com>
2020-11-27 14:52:05 +0200

    meniu

--- resources/views/contract/index.blade.php
+++ resources/views/contract/index.blade.php
@@ -46,6 +46,7 @@
 
         @endforeach
  </ul>
+ <a href="http://junior4.qik.ro/" class="m-5 py-1 px-1 bg-white-400 border cursor-pointer rounded text-black"> Back</a>
 
  
 @endsection
--- resources/views/log/index.blade.php
+++ resources/views/log/index.blade.php
@@ -149,9 +149,9 @@
 
 
         
-
  </ul>
  
+ <a href="http://junior4.qik.ro/" class="m-5 py-1 px-1 bg-white-400 border cursor-pointer rounded text-black"> Back</a>
 
  
 @endsection
